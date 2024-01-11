4bb253c62d22178f6211d581435d3078c48c2053
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-01-11 17:45:38 +0100

    Ajout de la possibilité de visualiser et d'éditer les infos du profil de l'utilisateur avec une nouvelle page ProfileView.vue

--- projet/src/views/ProfileView.vue
+++ projet/src/views/ProfileView.vue
@@ -1,23 +1,48 @@
 <template>
+  <button> <a href="/">Revenir à l'accueil</a></button>
   <form>
     <label for="name">Name</label>
-    <input type="text" id="name" name="name" />
+    <input type="text" id="name" name="name" v-model="user.name" />
     <label for="email">Email</label>
-    <input type="email" id="email" name="email" />
-    <button type="submit">Submit</button>
+    <input type="email" id="email" name="email" v-model="user.email" />
+    <button type="submit" @click="ModifProfil()">Enregistrer</button>
   </form>
 </template>
 
 <script>
+import axiosInstance from "../api/index.js";
+
 export default {
   name: "ProfileView",
   data() {
     return {
-      name: "",
-      email: "",
+      user: {}
     };
   },
-}
+  beforeRouteEnter(to, from, next) {
+    // faire appel à l'API pour récupérer les données de l'utilisateur
+    axiosInstance.get(`/api/profile`).then(response => {
+      console.log(response.data.name);
+      next (vm => {
+        vm.user.name = response.data.name;
+        vm.user.email  = response.data.email;
+      });
+    });
+  },
+  methods: {
+    ModifProfil() {
+      axiosInstance
+        .put(`/api/profile`, this.user)
+        .catch(error => {
+          console.log(error);
+        })
+        .then(response => {
+          console.log(response);
+        });
+    }
+  },
+
+};
 </script>
 
 <style scoped>

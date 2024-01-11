0ccc8b0ad7b4f8093ff22ade60eb85d5303b8e5f
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-01-11 16:45:49 +0100

    modif du script du routeur + ajout de la vue ProfileView.vue

---
+++ projet/src/views/ProfileView.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name" />
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ProfileView",
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>

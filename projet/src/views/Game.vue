529730b3282ce6b36d44a861a3c3f21f14c1a3de
toinou887 <rionde8u@etu.univ-lorraine.fr>
2024-01-18 17:53:12 +0100

    Modif de Game.vue (ajout des croix et des ronds)

--- projet/src/views/Game.vue
+++ projet/src/views/Game.vue
@@ -5,20 +5,26 @@
     <textarea v-model="game.code">Code de la partie</textarea>
   </header>
 
-    <div class="morpion">
-      <section @click="play(1,1)"></section>
-      <section @click="play(1,2)"></section>
-      <section @click="play(1,3)"></section>
-      <section @click="play(2,1)"></section>
-      <section @click="play(2,2)"></section>
-      <section @click="play(2,3)"></section>
-      <section @click="play(3,1)"></section>
-      <section @click="play(3,2)"></section>
-      <section @click="play(3,3)"></section>
-    </div>
+  <div class="morpion">
+    <section @click="play(1,1)"></section>
+    <section @click="play(1,2)"></section>
+    <section @click="play(1,3)"></section>
+    <section @click="play(2,1)"></section>
+    <section @click="play(2,2)"></section>
+    <section @click="play(2,3)"></section>
+    <section @click="play(3,1)"></section>
+    <section @click="play(3,2)"></section>
+    <section @click="play(3,3)"></section>
+  </div>
 
-   <footer>
-      <button> <a href="/">Revenir à l'accueil</a></button>
+  <div class="game-state">
+    <div v-if="game.state === 0">En attente d'un adversaire</div>
+    <div v-if="game.state === 1">En cours</div>
+    <div v-if="game.state === 2">Terminée, gagnant : <input v-model="game.winner.name" /> </div>
+  </div>
+
+  <footer>
+    <button> <a href="/">Revenir à l'accueil</a></button>
   </footer>
 
 </template>
@@ -29,10 +35,11 @@ export default {
   name: "GameVue",
   data() {
     return {
-      game: {owner: {}},
+      game: {owner: {}, winner: {}},
       symbols: {
         0: "X",
-        1: "O"}
+        1: "O"
+      }
     };
   },
   computed: {
@@ -45,7 +52,38 @@ export default {
     }
   },
   methods: {
-    async waitForOpponentMove(isFirstPlayerPlaying, row, col) {
+    makeGrid(userSymbol, row, col) {
+      switch (`${row},${col}`) {
+        case '1,1':
+          document.querySelector("section:nth-child(1)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '1,2':
+          document.querySelector("section:nth-child(2)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '1,3':
+          document.querySelector("section:nth-child(3)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '2,1':
+          document.querySelector("section:nth-child(4)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '2,2':
+          document.querySelector("section:nth-child(5)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '2,3':
+          document.querySelector("section:nth-child(6)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '3,1':
+          document.querySelector("section:nth-child(7)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '3,2':
+          document.querySelector("section:nth-child(8)").innerHTML = this.symbols[userSymbol];
+          break;
+        case '3,3':
+          document.querySelector("section:nth-child(9)").innerHTML = this.symbols[userSymbol];
+          break;
+      }
+    },
+    async waitForOpponentMove(row, col) {
       if (this.game.state === 2) {
         return;
       }
@@ -58,10 +96,11 @@ export default {
         }
       });
 
-     if (isFirstPlayerPlaying) {
-
-
-     }
+      if (response.data.next_player_id === this.game.owner_id) {
+        this.makeGrid(0, row, col);
+      } else {
+        this.makeGrid(1, row, col);
+      }
 
       this.game = response.data;
       if (this.game.state !== 2) {
@@ -75,8 +114,8 @@ export default {
       let params = this.$route.params;
       let id = params.id;
       axiosInstance.patch(`/api/games/${id}/play/${row}/${col}`).then(response => {
-
-
+        this.game = response.data;
+        this.waitForOpponentMove(row, col);
         console.log('okay, lets go!');
       }).catch(error => {
         console.log("La cellule cible a déjà été jouée");
@@ -92,6 +131,9 @@ export default {
         vm.game.opponent = response.data.opponent;
         vm.game.state = response.data.state;
         vm.game.last_time_updated = response.data.last_time_updated;
+        vm.game.winner.name = response.data.winner.name;
+        vm.game.next_player_id = response.data.next_player_id;
+        vm.game.owner_id = response.data.owner_id;
       });
     });
 
